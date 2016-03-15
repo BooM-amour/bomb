12119af83d37ac7fe79637aa2a0339f272759ae6
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-15 07:12:02 -0700

    Update server.lua

--- plugins/server.lua
+++ plugins/server.lua
@@ -1,41 +1,26 @@
 local function run(msg)
-if msg.text == "[!/]turn off" then
-	return "Please Wait 8Min\nServer Shuting Down..."
+if msg.text == "bot off" then
+	return "Please Wait \nServer Shuting Down..."
 end
-if msg.text == "[!/]server" then
-	return "https://212.33.207.97:2222"
+if msg.text == "server info" then
+	return "https://192.99.230.254:22"
+
 end
-if msg.text == "[!/]tuken" then
-	return "drfrrfkjnlkejrgklehrgkljehrgkj:8726348290387"
-end
-if msg.text == "[!/]login" then
-	return "https://umbrella.shayan-soft.ir:2222"
-end
-if msg.text == "[!/]reset" then
-	return "Are You Sure??"
-end
-if msg.text == "[!/]restart" then
+if msg.text == "reboot" then
 	return "Please Wait 8Min\nServer Restarting..."
 end
 end
-
 return {
 	description = "Server Switch and Access", 
 	usage = {
-		"/turn off : turn off server",
-		"/restart : restart server",
-		"/reset : delete database",
-        "/server : access server",
-		"/login : access server",
-		"/tuken : server tukrn",
+		"/bot off : turn off server",
+		"/reboot : restart server",
+        "/server info : access server",
 		},
 	patterns = {
-		"^[!/]turn? (off)",
-		"^[!/]restart$",
-		"^[!/]reset$",
-		"^[!/]server$",
-		"^[!/]login$",
-		"^[!/]tuken$",
+		"^[#]bot (off)",
+		"^[#]reboot$",
+		"^[#]server info$",
 		}, 
 	run = run,
     privileged = true,
